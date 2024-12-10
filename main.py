5f0c558332f4b42e636757da2485752007f2c5d7
Micah Howard <mhoward@westmont.edu>
2024-12-09 23:15:02 -0800

    Adjusted code to send a query to the product page and parse a title from the product page. Refactored code slightly

--- main.py
+++ main.py
@@ -1,58 +1,100 @@
 from openai import OpenAI
+from bs4 import BeautifulSoup
+from time import sleep
 import requests
 
-client = OpenAI()
+def main():
+    client = OpenAI()
 
-# System prompt which defines behavior for gpt-4o-mini
-conversation = [
-                {"role": "system", "content": "You are a gift idea giving assistant who takes in characteristics about a recipient and responds with five gift ideas in a comma-delimited list without categories or enumeration."},
-]
+    # System prompt which defines what behavior we want from gpt-4o-mini
+    conversation = [
+                    {"role": "system", "content": "You are a gift idea giving assistant who takes in characteristics about a recipient and responds with five gift ideas in a comma-delimited list without categories or enumeration."},
+    ]
 
-# A url we use to return Amazon search results later on.
-url = "https://www.searchapi.io/api/v1/search?api_key=KYq3CstXu71Bw5nBtaDUhixU"
+    # A url we use to return Amazon search results later on.
+    url = "https://www.searchapi.io/api/v1/search?api_key=KYq3CstXu71Bw5nBtaDUhixU"
 
-print("Hello! My name is Elfie and I am your gift helper for the holidays! ")
-while True:
-    # Funnels user input into bot
-    user_input = input("Please enter in who you want to get a gift for, and the characteristics of that person. :)\n")
-    conversation.append({"role": "user", "content": user_input})
-    
-    # Creates response choices from system prompt and user input
-    response = client.chat.completions.create(
-        model="gpt-4o-mini", 
-        messages = conversation
-    )
-    
-    # Takes the first choice response's content. Ignores the rest.
-    response_message = response.choices[0].message.content
-
-    conversation.append({"role": "assistant", "content": response_message})
-    #print("\n" + response_message + "\n")
-    
-    # Since the bot is returning comma delimited lists in strings, we can actual lists by splitting at the comma
-    response_list = response_message.split(',')
-
-    print("Thanks! Here are the gift ideas!\n")
-
-    print("-------------------------------------------------------------------")
-
-    for i in range(len(response_list)):
-        # Eliminates extra whitespace at start of string
-        response_list[i] = response_list[i].strip()
-        print(f"Gift idea {i}. {response_list[i]}\n")
-        params = {
-            "engine": "amazon_search",
-            "q": response_list[i]
-        }
+    print("Hello! My name is Elfie and I am your gift helper for the holidays! ")
+    while True:
+        # Funnels user input into bot
+        user_input = input("Please enter in who you want to get a gift for, and the characteristics of that person. :)\nEnter 'exit' to exit.\n")
+        if user_input.lower() == 'exit':
+            break
+        conversation.append({"role": "user", "content": user_input})
         
-        # Gets the url with amazon search and the gift idea from the list
-        response = requests.get(url, params = params).json()
+        # Creates response choices from system prompt and user input
+        response = client.chat.completions.create(
+            model="gpt-4o-mini", 
+            messages = conversation
+        )
+        
+        # Takes the first choice response's content. Ignores the rest.
+        response_message = response.choices[0].message.content
 
-        print("Here is a product I found that I think best fits the gift idea!\n")
-        # 'organic_results' gets the results of the search from the JSON object in response
-        print("Name: ", response['organic_results'][i]['title'])
-        print("Price", response['organic_results'][i]['price'])
-        print("Link", response['organic_results'][i]['link'])
-        print()
+        conversation.append({"role": "assistant", "content": response_message})
+        
+        # Since the bot is returning comma delimited lists in strings, we can actual lists by splitting at the comma
+        response_list = response_message.split(',')
+
+        print("Thanks! Here are the gift ideas!\n")
+
         print("-------------------------------------------------------------------")
 
+        for i in range(len(response_list)):
+            # Eliminates extra whitespace at start of string
+            response_list[i] = response_list[i].strip()
+            print(f"Gift idea {i+1}: {response_list[i]}\n")
+            params = {
+                "engine": "amazon_search",
+                "q": response_list[i]
+            }
+            
+            # Gets the url with amazon search and the gift idea from the list
+            search_response = get_response(url, params)
+            if search_response == None:
+                break
+            else:
+                search_response = search_response.json()
+                
+            # Gets the link to the product page, which will be used to find a title
+            product_link = search_response['organic_results'][i]['link']
+            product_link = link.rsplit('ref')[0]
+            
+            # Gets the html of the product page
+            product_response = get_response(product_link)
+
+            # Enforces politness
+            sleep(1)
+
+            # Parses the page and grabs the title
+            soup = BeautifulSoup(product_response.text, 'html.parser')
+            title = soup.title.string
+
+            # Removes 'Amazon.com :' from title
+            title = title.rsplit(': ')[1]
+            
+            # Simplifies common title pattern
+            title = title.rsplit('|')[0]
+
+            print("Here is a product I found that I think best fits the gift idea!\n")
+            # 'organic_results' gets the results of the search from the JSON object in response
+            print("Name: ", title)
+            print("Price", response['organic_results'][i]['price'])
+            print("Link", response['organic_results'][i]['link'])
+            print()
+            print("-------------------------------------------------------------------")
+
+
+def get_response(url, params = None):
+    try:
+        response = requests.get(url, params = params) 
+        # Raises an error if the HTTP code is an eror
+        response.raise_for_status()
+        return response
+    except requests.exceptions.HTTPError as errh:
+        print('HTTP Error')
+        print(errh.args[0])
+        return None
+     
+
+main()
